b49a5488ff4a5b90c15a4115509cc9f832717dad
Rapcuyyy <rafadarussalam9@gmail.com>
2026-09-08 07:45:23 +0700

    Tutorial 2

---
+++ main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "username": "Rapcuyyy",
+        "name": "Rafa Darussalam",
+        "npm": "2506538924",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "CS student at Universitas Indonesia"
+            " | Passionate about Software Engineering, Web Development, and Game Development"
+            " | Actively exploring diverse experiences, from teaching assistantships and student organizations to committees and internship opportunities."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Rafa Darussalam",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
