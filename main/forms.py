be5950ff81feecad5c8ed7ba0d17e9f40430225b
Rapcuyyy <rafadarussalam9@gmail.com>
2026-09-18 20:54:29 +0700

    membuat ModelForm baru di main/form.py (tahun masih disertakan untuk kemudahan menambahkan pendidikan terdahulu)

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm, TextInput, Textarea, URLInput, DateInput
+from django.forms import ModelForm, TextInput, Textarea, URLInput, NumberInput, FileInput
 
-from main.models import Experience
+from main.models import Experience, Education
 
 class ExperienceForm(ModelForm):
     class Meta:
@@ -40,6 +40,77 @@ class ExperienceForm(ModelForm):
             "thumbnail": URLInput(
                 attrs={
                     "placeholder": "https://drive.google.com/file/d/1OFZq-AKltIAK-i8o4xHScl64AchpgqKf/view?usp=sharing",
+                }
+            ),
+        }
+
+class EducationForm(ModelForm):
+    class Meta:
+        model = Education
+        # Mengecualikan 'id' karena bersifat non-editable (auto-generate)
+        fields = [
+            "place",
+            "major",
+            "category",
+            "year_start",
+            "year_grad",
+            "description",
+            "logo",
+            "thumbnail",
+        ]
+
+        labels = {
+            "place": "Institution Name",
+            "major": "Major / Field of Study",
+            "category": "Category",
+            "year_start": "Start Year",
+            "year_grad": "Graduation Year",
+            "description": "Description",
+            "logo": "Institution Logo",
+            "thumbnail": "Related Link / Certificate",
+        }
+
+        widgets = {
+            "place": TextInput(
+                attrs={
+                    "placeholder": "e.g., Universitas Indonesia",
+                    "maxlength": 225,
+                }
+            ),
+            "major": TextInput(
+                attrs={
+                    "placeholder": "e.g., Computer Science",
+                }
+            ),
+            "category": TextInput(
+                attrs={
+                    "placeholder": "elementary, junior, senior, bachelor, other",
+                }
+            ),
+            "year_start": NumberInput(
+                attrs={
+                    "placeholder": "e.g., 2022",
+                }
+            ),
+            "year_grad": NumberInput(
+                attrs={
+                    "placeholder": "e.g., 2026 (Leave blank if present)",
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Tell us about your education journey",
+                    "rows": 3,
+                }
+            ),
+            "logo": FileInput(
+                attrs={
+                    "class": "project-search__input",
+                }
+            ),
+            "thumbnail": URLInput(
+                attrs={
+                    "placeholder": "Website where you learn",
                 }
             ),
         }
